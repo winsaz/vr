d1c846e1d7a83f60b30ad286309e1e531074f4f9
yoasfs <yoas.febrianus@qraved.com>
2017-04-17 11:21:46 +0700

    update git ignore, add test image

--- app/src/main/java/com/winston/vrproject/MainActivity.java
+++ app/src/main/java/com/winston/vrproject/MainActivity.java
@@ -1,7 +1,12 @@
 package com.winston.vrproject;
 
+import android.content.res.AssetManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,29 +16,58 @@ import com.bumptech.glide.request.animation.GlideAnimation;
 import com.bumptech.glide.request.target.SimpleTarget;
 import com.google.vr.sdk.widgets.pano.VrPanoramaView;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.ref.WeakReference;
+
 public class MainActivity extends AppCompatActivity {
     VrPanoramaView panoramaView;
+    private ImageLoaderTask backgroundImageLoaderTask;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         panoramaView = (VrPanoramaView)findViewById(R.id.pano_view);
-        VrPanoramaView.Options options = new VrPanoramaView.Options();
-        String panoImageUrl = "https://upload.wikimedia.org/wikipedia/commons/6/6f/Helvellyn_Striding_Edge_360_Panorama%2C_Lake_District_-_June_09.jpg";
-        Glide.with(this).load(getResources().getDrawable(R.drawable.1)).asBitmap().into(new SimpleTarget<Bitmap>() {
-            @Override
-            public void onResourceReady(Bitmap resource, GlideAnimation<? super Bitmap> glideAnimation) {
-                if (resource != null) {
-                    panoramaView.loadImageFromBitmap(resource, new VrPanoramaView.Options());
-                }
-            }
 
-            @Override
-            public void onLoadFailed(Exception e, Drawable errorDrawable) {
-                super.onLoadFailed(e, errorDrawable);
-                Log.v("Glide Error", "Error : " + e.getLocalizedMessage());
-            }
-        });
+        ImageLoaderTask task = backgroundImageLoaderTask;
+        if (task != null && !task.isCancelled()) {
+            // Cancel any task from a previous loading.
+            task.cancel(true);
+        }
+
+        // pass in the name of the image to load from assets.
+        VrPanoramaView.Options viewOptions = new VrPanoramaView.Options();
+        viewOptions.inputType = VrPanoramaView.Options.TYPE_STEREO_OVER_UNDER;
+
+        // use the name of the image in the assets/ directory.
+        String panoImageName = "test_1.jpg";
+
+        // create the task passing the widget view and call execute to start.
+        task = new ImageLoaderTask(panoramaView, viewOptions, panoImageName);
+        task.execute(this.getAssets());
+        backgroundImageLoaderTask = task;
+//        final VrPanoramaView.Options viewOptions = new VrPanoramaView.Options();
+//        viewOptions.inputType = VrPanoramaView.Options.TYPE_STEREO_OVER_UNDER;
+//        String panoImageUrl = "http://cdn1.360cities.net/pano/rami-saarikorpi/00574590_Kauppatori.jpeg/equirect_crop_3_1/5.jpg";
+//        Glide.with(this).load(panoImageUrl).asBitmap().into(new SimpleTarget<Bitmap>() {
+//            @Override
+//            public void onResourceReady(Bitmap resource, GlideAnimation<? super Bitmap> glideAnimation) {
+//                Log.v("Glide Error", "Resource Ready");
+//                if (resource != null) {
+//                    Log.v("Glide Error", "Resource Loaded");
+//                    panoramaView.loadImageFromBitmap(resource, viewOptions);
+//                } else {
+//                    Log.v("Glide Error", "Resource Null");
+//                }
+//            }
+//
+//            @Override
+//            public void onLoadFailed(Exception e, Drawable errorDrawable) {
+//                super.onLoadFailed(e, errorDrawable);
+//                Log.v("Glide Error", "Error : " + e.getLocalizedMessage());
+//            }
+//        });
     }
 
     @Override
@@ -51,5 +85,47 @@ public class MainActivity extends AppCompatActivity {
     public void onDestroy() {
         panoramaView.shutdown();
         super.onDestroy();
+    }
+
+
+
+    public class ImageLoaderTask extends AsyncTask<AssetManager, Void, Bitmap> {
+        private static final String TAG = "ImageLoaderTask";
+        private final String assetName;
+        private final WeakReference<VrPanoramaView> viewReference;
+        private final VrPanoramaView.Options viewOptions;
+        private WeakReference<Bitmap> lastBitmap = new WeakReference<>(null);
+        private String lastName;
+        public ImageLoaderTask(VrPanoramaView view, VrPanoramaView.Options viewOptions, String assetName) {
+            viewReference = new WeakReference<>(view);
+            this.viewOptions = viewOptions;
+            this.assetName = assetName;
+        }
+        @Override
+        protected Bitmap doInBackground(AssetManager... params) {
+            AssetManager assetManager = params[0];
+
+            if (assetName.equals(lastName) && lastBitmap.get() != null) {
+                return lastBitmap.get();
+            }
+
+            try(InputStream istr = assetManager.open(assetName)) {
+                Bitmap b = BitmapFactory.decodeStream(istr);
+                lastBitmap = new WeakReference<>(b);
+                lastName = assetName;
+                return b;
+            } catch (IOException e) {
+                Log.e(TAG, "Could not decode default bitmap: " + e);
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            final VrPanoramaView vw = viewReference.get();
+            if (vw != null && bitmap != null) {
+                vw.loadImageFromBitmap(bitmap, viewOptions);
+            }
+        }
     }
 }
